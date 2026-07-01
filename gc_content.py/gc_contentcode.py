6033a318847523e8b3a23efa0a6316acb6abc3a1
Hanna Melek Henry <thewaysofthegeek@gmail.com>
2026-07-01 15:40:29 +0530

    plotting blast hits

--- gc_content.py/gc_contentcode.py
+++ gc_content.py/gc_contentcode.py
@@ -147,11 +147,29 @@ SeqIO.write(query_record,
 
 #%%
 #reading blast results
+columns = [
+    "Query",
+    "Subject",
+    "Identity",
+    "Alignment_Length",
+    "Mismatches",
+    "Gap_Openings",
+    "Query_Start",
+    "Query_End",
+    "Subject_Start",
+    "Subject_End",
+    "E-value",
+    "Bit_Score"
+]
+
 blast_df = pd.read_csv(
-    r"C:\Users\Dr.MAAM\Desktop\GC Content Genome Analyser\output\BLAST_hit.csv"
+    r"C:\Users\Dr.MAAM\Desktop\GC Content Genome Analyser\output\BLAST_hit.csv",
+    header=None,
+    names=columns
 )
-print(blast_df.head())
-                       
+
+print(blast_df.columns)
+
 # %%
 import numpy as np
 #converting E-value to LogE for better visualization
@@ -160,6 +178,25 @@ blast_df["E-value"] = blast_df["E-value"].replace(0, 1e-300)
 blast_df["LogE"] = -np.log10(blast_df["E-value"])
 
 #plotting blast hits
-plt.figure(figsize=(10,5))
-plt.bar(blast_df["Organism"], blast_df["LogE"])
+plt.figure(figsize=(20,6))
+plt.bar(blast_df["Subject"], blast_df["LogE"])
+plt.xticks(rotation=45, ha="right")
+plt.xlabel("Subject")
+plt.ylabel("-log10(E-value)")
+plt.title("Top Blast Hits for Bacillus subtilis Query")
+plt.tight_layout()
+plt.savefig(r"C:\Users\Dr.MAAM\Desktop\GC Content Genome Analyser\output\BLAST_hit.png"
+)
+plt.show()
 
+# %%
+print(blast_df.columns.tolist())
+print(blast_df.head())
+
+# %%
+
+
+
+# %%
+
+# %%
